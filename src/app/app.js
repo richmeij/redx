44b09311ebfb1cd5493110b2a2375d70c10594eb
Richard Meijer <richard.meijer@gmail.com>
2018-07-24 21:59:25 +0200

    Fixed packages

---
+++ src/app/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { observer } from '../lib/redx-react';
+import countStore from './countStore';
+
+// Create our app
+export const App = ({ countStore, title }) => {
+    const { counter, delay, increment, decrement, incrementAsync } = countStore;
+    const disabled = delay > 0;
+    return (
+        <React.Fragment>
+            <h1>{ title }</h1>
+            <p>
+                <button onClick={() => decrement()} disabled={disabled}>-</button>
+                &nbsp;<em>{ counter }</em>&nbsp;
+                <button onClick={() => increment()} disabled={disabled}>+</button>
+            </p>
+            <p>
+                <button onClick={() => incrementAsync()} disabled={disabled}>Async + (5 decond delay)</button>
+            </p>
+            <p>
+                { delay > 0
+                    ? <span>Fetching next increment. ETA {delay} seconds...</span>
+                    : null
+                }
+            </p>
+        </React.Fragment>
+    );
+};
+
+export default observer(countStore)(App);
--- src/app/bundle.js
+++ src/app/bundle.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import countStore from './countStore';
-import { observer } from '../lib/redx-react';
+import App from './app';
 
 // Create store
 const store = createStore(
@@ -12,38 +12,10 @@ const store = createStore(
     applyMiddleware(thunk)
 );
 
-// Create our app
-const App = ({ countStore, title }) => {
-    const { counter, delay, increment, decrement, incrementAsync } = countStore;
-    const disabled = delay > 0;
-    return (
-        <React.Fragment>
-            <h1>{ title }</h1>
-            <p>
-                <button onClick={() => decrement()} disabled={disabled}>-</button>
-                &nbsp;<em>{ counter }</em>&nbsp;
-                <button onClick={() => increment()} disabled={disabled}>+</button>
-            </p>
-            <p>
-                <button onClick={() => incrementAsync()} disabled={disabled}>Async + (5 decond delay)</button>
-            </p>
-            <p>
-                { delay > 0
-                    ? <span>Fetching next increment. ETA {delay} seconds...</span>
-                    : null
-                }
-            </p>
-        </React.Fragment>
-    );
-};
-
-// Create a version of our app, which observes the countStore
-const ObserverApp = observer(countStore)(App);
-
 // render the app in the DOM
 render(
         <Provider store={store}>
-            <ObserverApp title="count demo" />
+            <App title="count demo" />
         </Provider>
     , document.querySelector('#root')
 );
